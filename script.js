7af5f90e1512c118c343937088ccb2ff704aa290
Luke Leedy <lukeleedy123@gmail.com>
2022-07-23 11:39:46 -0400

    Add leaderboard and best time prompt

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ const KEYS = "abcdefghijklmnopqrstuvwxyz";
 const textArea = document.querySelector("#text-area");
 const startButton = document.querySelector("#start");
 const prompt = document.querySelector("#prompt");
+const leaderboard = document.querySelector(".leaderboard");
 const countdownLength = 3;
 
+let bestTime = Infinity;
 let randomLetter = null;
 let timerStart;
 
 textArea.addEventListener("keydown", (event) => {
   const key = event.key;
   let reaxionTime;
+
   textArea.value = "";
   if (key.toLowerCase() === randomLetter) {
     reaxionTime = (new Date().getTime() - timerStart) / 1000;
-    setPrompt(`Nice! Your Reaxion time is ${reaxionTime} seconds.`);
+    setPrompt(getPromptFromTime(reaxionTime));
     startButton.style.visibility = "visible";
     startButton.value = "Retry? [Press R]";
     randomLetter = null;
@@ -42,9 +45,42 @@ function start() {
   }
 }
 
+function getPromptFromTime(time) {
+  let promptPrefix;
+
+  if (setBestTime(time)) {
+    promptPrefix = "New record!";
+  } else if (time > 15) {
+    promptPrefix = "Did you fall asleep at the keyboard? ";
+  } else if (time > 7) {
+    promptPrefix = "Awful. ";
+  } else if (time > 3) {
+    promptPrefix = "Bad. ";
+  } else if (time > 1) {
+    promptPrefix = "Not great. ";
+  } else if (time > 0.5) {
+    promptPrefix = "Good. ";
+  } else if (time > 0.4) {
+    promptPrefix = "Great. ";
+  } else {
+    promptPrefix = "Awesome! ";
+  }
+
+  return promptPrefix + ` Your Reaxion time is ${time} seconds.`;
+}
+
 function setPrompt(message) {
   console.log(message);
   prompt.innerHTML = message;
+}
+
+function setBestTime(time) {
+  if (time < bestTime) {
+    bestTime = time;
+    leaderboard.innerHTML = `Best time: ${bestTime}`;
+    return true;
+  }
+  return false;
 }
 
 function countdownEnd() {
